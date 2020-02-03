d78d0046089455322358efd9e3cf6176b930253f
Ola mohammed <ola10-93@hotmail.com>
2020-02-03 11:55:31 +0400

    changed new things

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("hi am ola."); 
+  res.send("hi am ola Mohammed."); 
 });
 
 app.listen(3000, () => console.log("server is running on 3000"));
